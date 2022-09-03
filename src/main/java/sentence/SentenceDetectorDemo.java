9346a71a8c8d100140174d67997cadf8dca5add9
nhantruongvn <108405814+nhantruongvn@users.noreply.github.com>
2022-09-03 14:27:33 +0700

    chore

--- src/main/java/sentence/SentenceDetectorDemo.java
+++ src/main/java/sentence/SentenceDetectorDemo.java
@@ -7,7 +7,6 @@ import opennlp.tools.util.Span;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,14 +15,19 @@ public class SentenceDetectorDemo {
         Logger logger = Logger.getLogger(SentenceDetectorDemo.class.getName());
         logger.setLevel(Level.INFO);
 
+        String text = "What can art teach you about the person who made it? Maybe you’ll learn about their favorite colors. Maybe you’ll learn what shapes they find interesting. Maybe you’ll learn about an emotion they felt, or wanted other people to feel. Or maybe you’ll learn about part of the artist’s culture that they wanted to share with you. ";
+
         try (InputStream modelIn = new FileInputStream("src/main/resources/models/opennlp-en-ud-ewt-sentence-1.0-1.9.3.bin")) {
             SentenceModel model = new SentenceModel(modelIn);
             SentenceDetectorME sentenceDetector = new SentenceDetectorME(model);
-            String sentences[] = sentenceDetector.sentDetect("  What can art teach you about the person who made it? Maybe you’ll learn about their favorite colors. Maybe you’ll learn what shapes they find interesting. Maybe you’ll learn about an emotion they felt, or wanted other people to feel. Or maybe you’ll learn about part of the artist’s culture that they wanted to share with you. ");
+            logger.info("Detected sentences successfully.");
+            String sentences[] = sentenceDetector.sentDetect(text);
+            System.out.println("There are " + sentences.length + " sentences from the input text:");
             for(String sentence : sentences)
                 System.out.println(sentence);
 
-            Span positions[] = sentenceDetector.sentPosDetect("  What can art teach you about the person who made it? Maybe you’ll learn about their favorite colors. Maybe you’ll learn what shapes they find interesting. Maybe you’ll learn about an emotion they felt, or wanted other people to feel. Or maybe you’ll learn about part of the artist’s culture that they wanted to share with you. ");
+            Span positions[] = sentenceDetector.sentPosDetect(text);
+
             for(Span position : positions)
                 System.out.println(position);
 
